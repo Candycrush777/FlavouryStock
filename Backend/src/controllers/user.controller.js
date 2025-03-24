2aabe9fe2cd48183d84d8d38da9c5f2d8c8c2857
AleBonilla30 <ale.bonilla309@gmail.com>
2025-03-24 18:27:56 +0100

    Modificacion de backend

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -74,6 +74,19 @@ exports.deleteUser = (req, res) =>{
     })
 }
 
+exports.getUser = (req, res)=>{
+    sql = "SELECT * FROM Usuarios "
+    db.query(sql, (err, result) =>{
+        if (err) {
+            return res.status(500).json({error: err.message})
+        }
+        if (result.length === 0) {
+            return res.status(404).json({error: "Usuario no encontrado"})
+        }
+        res.status(200).json(result)
+    })
+}
+
 
 exports.getUserId = (req, res)=>{
     const {id} = req.params

440a8837e7b7da278f4e5197d42c8d70a08371f8
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2025-04-05 03:26:50 +0200

    funciones user  para leer en front

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -6,7 +6,7 @@ const userController = require('../controllers/user.controller')
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.delete('/delete/:id', userController.deleteUser)
-router.get('/getUsers', userController.getUser)//obtiene todos los usuarios
+router.get('/getUser', userController.getUser)//obtiene todos los usuarios
 
 //update de usuario
 router.get('/edit/:id', userController.getUserId)//se obtiene los datos de usuario

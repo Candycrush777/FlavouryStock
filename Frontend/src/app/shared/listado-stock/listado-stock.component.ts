dfb60fcc4b6919373cdd1933d4038f6011ee9fd6
Candykush <eduardoafan.eae@gmail.com>
2025-04-13 20:31:59 +0200

    iconos querys 650px

--- Frontend/src/app/shared/listado-stock/listado-stock.component.ts
+++ Frontend/src/app/shared/listado-stock/listado-stock.component.ts
@@ -14,88 +14,97 @@ addProduct() {
 throw new Error('Method not implemented.');
 }
 
-  stockItems = [
-    {
-      alimento: {
-        nombre: 'Arroz',
-        foto: 'assets/arroz.jpg',
-      },
-      id: '#ARZ-001',
-      peso: '500g',
-      categoria: 'Cereal',
-      estado: 'En Stock'
+stockItems = [
+  {
+    alimento: {
+      nombre: 'Arroz',
+      foto: 'assets/arroz.jpg',
     },
-    {
-      alimento: {
-        nombre: 'Frijoles',
-        foto: 'https://img.freepik.com/fotos-premium/frijoles-rojos_64161-174.jpg',
-      },
-      id: '#FRJ-045',
-      peso: '1kg',
-      categoria: 'Legumbre',
-      estado: 'Sin Stock'
+    id: '#ARZ-001',
+    almacen: 5,
+    nevera: 3,
+    congelador: 2,
+    estado: 'En Stock'
+  },
+  {
+    alimento: {
+      nombre: 'Frijoles',
+      foto: 'https://img.freepik.com/fotos-premium/frijoles-rojos_64161-174.jpg',
     },
-    {
-      alimento: {
-        nombre: 'Harina',
-        foto: 'assets/harina.jpg',
-      },
-      id: '#HRN-112',
-      peso: '2kg',
-      categoria: 'Harina',
-      estado: 'Pedido'
+    id: '#FRJ-045',
+    almacen: 0,
+    nevera: 0,
+    congelador: 0,
+    estado: 'Sin Stock'
+  },
+  {
+    alimento: {
+      nombre: 'Harina',
+      foto: 'assets/harina.jpg',
     },
-    {
-      alimento: {
-        nombre: 'Azúcar',
-        foto: 'https://imagenes.heraldo.es/files/image_1920_1080/uploads/imagenes/2025/01/09/azucar-gsc1.jpeg',
-      },
-      id: '#AZC-204',
-      peso: '750g',
-      categoria: 'Endulzante',
-      estado: 'En Stock'
+    id: '#HRN-112',
+    almacen: 4,
+    nevera: 1,
+    congelador: 0,
+    estado: 'Pedido'
+  },
+  {
+    alimento: {
+      nombre: 'Azúcar',
+      foto: 'https://imagenes.heraldo.es/files/image_1920_1080/uploads/imagenes/2025/01/09/azucar-gsc1.jpeg',
     },
-    {
-      alimento: {
-        nombre: 'Macarrones',
-        foto: 'assets/macarrones.jpg',
-      },
-      id: '#MCR-145',
-      peso: '1.5kg',
-      categoria: 'Cereal',
-      estado: 'Sin Stock'
+    id: '#AZC-204',
+    almacen: 3,
+    nevera: 2,
+    congelador: 1,
+    estado: 'En Stock'
+  },
+  {
+    alimento: {
+      nombre: 'Macarrones',
+      foto: 'assets/macarrones.jpg',
     },
-    {
-      alimento: {
-        nombre: 'Café',
-        foto: 'assets/cafe.jpg',
-      },
-      id: '#CAF-103',
-      peso: '500g',
-      categoria: 'Bebida',
-      estado: 'En Stock'
+    id: '#MCR-145',
+    almacen: 0,
+    nevera: 0,
+    congelador: 0,
+    estado: 'Sin Stock'
+  },
+  {
+    alimento: {
+      nombre: 'Café',
+      foto: 'assets/cafe.jpg',
     },
-    {
-      alimento: {
-        nombre: 'Carne Roja',
-        foto: 'assets/carneRoja.jpg',
-      },
-      id: '#CRJ-201',
-      peso: '1kg',
-      categoria: 'Carne',
-      estado: 'En Stock'
+    id: '#CAF-103',
+    almacen: 2,
+    nevera: 1,
+    congelador: 1,
+    estado: 'En Stock'
+  },
+  {
+    alimento: {
+      nombre: 'Carne Roja',
+      foto: 'assets/carneRoja.jpg',
     },
-    {
-      alimento: {
-        nombre: 'Salmón',
-        foto: 'assets/salmon.jpg',
-      },
-      id: '#SLM-501',
-      peso: '1kg',
-      categoria: 'Pescado',
-      estado: 'En Stock'
+    id: '#CRJ-201',
+    almacen: 0,
+    nevera: 3,
+    congelador: 2,
+    estado: 'En Stock'
+  },
+  {
+    alimento: {
+      nombre: 'Salmón',
+      foto: 'assets/salmon.jpg',
     },
-  ];
+    id: '#SLM-501',
+    almacen: 0,
+    nevera: 2,
+    congelador: 3,
+    estado: 'En Stock'
+  },
+];
+
  
   isFormVisible: boolean = false;  
   isTableVisible: boolean = false; 

6f0fc52509a066f1aebdddb7b9638ec7f2d719b3
Candykush <eduardoafan.eae@gmail.com>
2025-04-25 20:59:56 +0200

    cambios reunion

---
+++ Frontend/src/app/pipes/unidad-medida.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'unidadMedida',
+  standalone: false
+})
+export class UnidadMedidaPipe implements PipeTransform {
+  transform(value: string): string {
+    switch (value) {
+      case 'kilos':
+        return 'Kg';
+      case 'gramos':
+        return 'Gr';
+      case 'litros':
+        return 'Lt';
+      default:
+        return value;
+    }
+  }
+}
